ee1de82499a2295042b15f03fc0facf06e574340
HEY1MIK <kwon5943@gmail.com>
2022-08-17 01:00:06 +0900

    연습커밋

---
+++ InflernCote/src/inflern/algorithm/cote/sec1/Prac3.java
@@ -0,0 +1,11 @@
+package inflern.algorithm.cote.sec1;
+
+public class Prac3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("메인입니다.");
+	}
+
+}
