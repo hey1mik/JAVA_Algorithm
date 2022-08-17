d7601b76bff83cc923dcf28478cb8e7e97d61ca2
HEY1MIK <kwon5943@gmail.com>
2022-08-18 00:11:26 +0900

    수정 테스트

--- InflernCote/src/inflern/algorithm/cote/sec1/Prac3.java
+++ InflernCote/src/inflern/algorithm/cote/sec1/Prac3.java
@@ -5,7 +5,7 @@ public class Prac3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("메인입니다.");
+		System.out.println("이번엔 수정 테스트를 해볼거예요.");
 	}
 
 }
