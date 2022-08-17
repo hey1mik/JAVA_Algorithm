ff422669664f47e7e0c0c93e9dfe0b6984e81da5
HEY1MIK <kwon5943@gmail.com>
2022-08-18 00:10:29 +0900

    또 다른 테스트..

---
+++ InflernCote/src/inflern/algorithm/cote/sec1/Prac5.java
@@ -0,0 +1,11 @@
+package inflern.algorithm.cote.sec1;
+
+public class Prac5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("또 다른 테스트");
+	}
+
+}
